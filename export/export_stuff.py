341b441753c8c3caa454c499fe82e43b09131402
drasich <drasich@gmail.com>
2012-12-20 13:57:33 +0900

    texture

--- export/export_stuff.py
+++ export/export_stuff.py
@@ -33,7 +33,9 @@ class Mesh:
 def get_triangles(mesh_data, uvs, vv):
   triangles = []
   #uvs = []
-  uvdata = mesh_data.uv_layers[0].data
+  uvdata = []
+  if len(mesh_data.uv_layers) > 0:
+    uvdata = mesh_data.uv_layers[0].data
   print("tess faces length : " + str(len(mesh_data.tessfaces)))
   i = 0
   uvc = 0
@@ -42,17 +44,18 @@ def get_triangles(mesh_data, uvs, vv):
     triangles.append((vertices[0], vertices[1], vertices[2]))
     #print("vertice " + vertices[0] + " get this : " + vertices[0]
     print(str(i) + " append this " + str(vertices[0]) + str(vertices[1]) +str(vertices[2]))
-    print(" with uv " + str(uvdata[uvc].uv) + str(uvdata[uvc+1].uv) +str(uvdata[uvc+2].uv))
-    uvs[vertices[0]] = uvdata[uvc].uv
-    uvs[vertices[1]] = uvdata[uvc+1].uv
-    uvs[vertices[2]] = uvdata[uvc+2].uv
+    if uvdata:
+      print(" with uv " + str(uvdata[uvc].uv) + str(uvdata[uvc+1].uv) +str(uvdata[uvc+2].uv))
+      uvs[vertices[0]] = uvdata[uvc].uv
+      uvs[vertices[1]] = uvdata[uvc+1].uv
+      uvs[vertices[2]] = uvdata[uvc+2].uv
     i +=1
     uvc+=3
     # It's a quad we need one more triangle.
     if len(vertices) == 4:
       triangles.append((vertices[0], vertices[2], vertices[3]))
-      #uvs.append(uvdata[vertices[3]].uv)
-      uvs[vertices[3]] = uvdata[uvc].uv
+      if uvdata:
+        uvs[vertices[3]] = uvdata[uvc].uv
       i +=1
       uvc+=1
 
@@ -70,27 +73,6 @@ def get_normals(mesh_data):
     normals.append(v.normal)
   return normals
 
-def get_uvs(mesh_data):
-  uvs = []
-  #print("uv : " + str(mesh_data.uv_layers[0].data)) 
-  #print("************ : " + str(dir(mesh_data)))
-  uvs.append([1,0]) #d.uv)
-  uvs.append([0,0]) #d.uv)
-  uvs.append([1,1]) #d.uv) 2
-  uvs.append([0,1]) #d.uv)
-  uvs.append([0.5,0]) #d.uv)
-  uvs.append([0.5,1]) #d.uv)
-  uvs.append([1,0.5]) #d.uv) 6
-  uvs.append([0,0.5]) #d.uv)
-  uvs.append([0.5,0.5]) #d.uv) 8
-  #uvs.append([0,1]) #d.uv)
-
-  for d in mesh_data.uv_layers[0].data:
-    print("yo : " + str(d.uv))
-    #uvs.append(d.uv)
-  return uvs
-
-
 def create_mesh(mesh_data):
   mesh_data.update(calc_tessface=True)
   mesh = Mesh(mesh_data)
@@ -102,7 +84,6 @@ def create_mesh(mesh_data):
   print("triangles length : " + str(len(mesh.triangles)))
   #mesh.triangles = get_triangles(mesh_data)
   mesh.normals = get_normals(mesh_data)
-  #mesh.uvs = get_uvs(mesh_data)
 
   return mesh
 
@@ -129,5 +110,5 @@ def write_material(file, mat):
   at =  mat.active_texture
   print("texture : " + str(at))
   print("scale : " + str(mat.texture_slots[0].scale))
-  print("filepath : " + bpy.data.textures[at.name].image.filepath)
-  pass
+  if bpy.data.textures[at.name].type == 'IMAGE':
+    print("filepath : " + bpy.data.textures[at.name].image.filepath)
