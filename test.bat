a5d591d5d692bf7c7b31f78e5262a892cbfd8171
drasich <drasich@gmail.com>
2013-01-03 10:56:38 +0900

    windows command

--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 #blender.exe --background animtest.blend --python background.py
-"c:/Program Files/Blender Foundation/Blender/blender.exe" --background animtest.blend --python background.py
+#"c:/Program Files/Blender Foundation/Blender/blender.exe" --background animtest.blend --python background.py
+"c:/Program Files/Blender Foundation/Blender/blender.exe" --background $1 --python background.py
 
