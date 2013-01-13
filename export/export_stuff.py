6c22ed7ba08893f0f8ca20921823befab0be1dbb
chris <chris@indefini.org>
2013-01-13 12:19:23 +0900

    test

--- export/export_stuff.py
+++ export/export_stuff.py
@@ -25,6 +25,10 @@ class Action:
 
 from bpy.types import PoseBone
 def export_action(object, action):
+  a = Action(action.name)
+  print("action name " + action.name)
+  print("action frame range " + str(action.frame_range))
+
   #to get a frame/value couple
   #action.fcurves[1].keyframe_points[0].co
 
@@ -32,18 +36,27 @@ def export_action(object, action):
   pr = object.path_resolve
   for fcu in action.fcurves:
     try:
+      print("curve data path : " + fcu.data_path)
       prop = pr(fcu.data_path, False)
     except:
       prop = None
 
     if prop is not None:
       print("object " + object.name + " has action " + action.name + " with prop : " + str(prop))
+      print("dir prop : " + str(dir(prop.data)))
       print("and prop data is : " + str(prop.data))
       print("and prop data name is : " + str(prop.data.name))
+      print("and prop type is : " + str(type(prop)))
+
       if isinstance(prop.data, PoseBone):
         print("bone associated : " + str(prop.data.bone))
         print("bone group : " + str(prop.data.bone_group))
         print("posebone basename  : " + str(prop.data.basename))
+
+        #j'ai besoin de :
+        # bone name
+        # savoir ce que c'est genre position rotation x, y z w
+        #
     #else:
     #  print("object " + object.name + " has not action " + action.name)
   pass
